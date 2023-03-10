10da89ba041d053f6d6dbb6048fbf9ee19e4e6b1
rohancodex <rohancodex@gmail.com>
2023-03-10 09:55:03 +0530

    made image and link components

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx(), image({
+    serviceEntryPoint:'@astrojs/image/sharp'
+  })]
 });
