fea6740411a28dd58edec4e18b2d8df90f5e2931
rohancodex <77398907+rohancodex@users.noreply.github.com>
2023-06-16 03:51:24 +0530

    added nav items

--- src/data/navData.ts
+++ src/data/navData.ts
@@ -1,11 +1,19 @@
 const navData = [
   {
+    name: "Home",
+    path: "/",
+  },
+  {
     name: "About",
     path: "/about/",
   },
   {
-    name:"Blog",
-    path:"/blog/"
-  }
+    name: "Blog",
+    path: "/blog/",
+  },
+  {
+    name: "Projects",
+    path: "/projects/",
+  },
 ];
 export default navData;
